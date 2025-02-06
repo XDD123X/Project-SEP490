9edfce01ed7ecbd4e7ba9acf3d69dfb501ae666f
quana <quanhdpt@gmail.com>
2025-02-06 14:58:03 +0700

    remove attrubite from user username

--- BusinessObject/DTOs/UserAccountDTO.cs
+++ BusinessObject/DTOs/UserAccountDTO.cs
@@ -10,7 +10,6 @@ namespace BusinessObject.DTOs
     {
         public string FullName { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
         public string Role { get; set; }
         public string Password { get; set; }
     }
--- BusinessObject/Models/User.cs
+++ BusinessObject/Models/User.cs
@@ -8,7 +8,6 @@ public partial class User
     public Guid UserId { get; set; }
 
     public string Email { get; set; } = null!;
-    public string UserName { get; set; } = null!;
 
     public string Password { get; set; } = null!;
 
