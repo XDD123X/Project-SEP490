656d85d3a57ab5915174ac826b67afabe99f98b7
tiennqhe176827 <you@example.comtiennqhe176827@fpt.edu.vn>
2025-02-11 15:40:41 +0700

    update database

---
+++ OTMS.sql
@@ -0,0 +1,286 @@
+﻿-- Xoá và tạo lại cơ sở dữ liệu
+USE master;
+GO
+
+IF EXISTS (SELECT name FROM sys.databases WHERE name = 'OTMS')
+BEGIN
+    ALTER DATABASE OTMS SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE OTMS;
+END;
+GO
+
+CREATE DATABASE OTMS;
+GO
+
+USE OTMS;
+GO
+
+-- 1. Tạo bảng Role
+CREATE TABLE Role (
+    role_id uniqueidentifier  PRIMARY KEY DEFAULT NEWID(),
+    name NVARCHAR(50) NOT NULL UNIQUE,
+    description NVARCHAR(255) NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT NULL,
+    status INT DEFAULT 1
+);
+GO
+
+-- 2. Tạo bảng Account
+CREATE TABLE Account (
+    account_id uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+    email NVARCHAR(100) NOT NULL UNIQUE,
+    password NVARCHAR(255) NOT NULL,
+    full_name NVARCHAR(100) NOT NULL,
+    role_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Role(role_id),
+    fulltime BIT NULL DEFAULT 1,
+    phone_number NVARCHAR(15) NULL,
+    img_url NVARCHAR(500) DEFAULT 'https://dummyimage.com/400x600',
+	status INT NOT NULL DEFAULT 0,
+    created_at DATETIME DEFAULT GETDATE() ,
+    updated_at DATETIME DEFAULT NULL
+);
+GO
+
+-- 3. Tạo bảng Course
+CREATE TABLE Course (
+    course_id INT PRIMARY KEY IDENTITY(1,1),
+    course_name NVARCHAR(100) NOT NULL UNIQUE,
+    description NVARCHAR(255),
+    created_by uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Account(account_id),
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME NULL,
+    status INT DEFAULT 1
+);
+GO
+
+-- 4. Tạo bảng Class
+CREATE TABLE Class (
+    class_id uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+    class_code NVARCHAR(50) NOT NULL,
+    class_name NVARCHAR(100) NOT NULL,
+    course_id INT NOT NULL FOREIGN KEY REFERENCES Course(course_id),
+    total_session INT NOT NULL,
+    start_date DATETIME NULL,
+    end_date DATETIME NULL,
+	class_url NVARCHAR(100) NULL DEFAULT 'https://example.com/meet/euf-nwbu-cet',
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME NULL,
+    status INT DEFAULT 1
+);
+GO
+
+-- 5. Tạo bảng Session
+CREATE TABLE Session (
+    session_id uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+    class_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Class(class_id),
+    lecturer_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Account(account_id),
+    session_date DATETIME NOT NULL,
+    slot INT NOT NULL,
+    description NVARCHAR(255),
+    session_record DATE NULL,
+	type INT DEFAULT 1,
+    status INT DEFAULT 1,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME NULL
+);
+GO
+
+-- 6. Tạo bảng SessionRecord
+CREATE TABLE SessionRecord (
+    record_id uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+    session_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Session(session_id),
+    video_url NVARCHAR(500),
+    description NVARCHAR(255),
+    uploaded_by NVARCHAR(255),
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME NULL,
+    status INT DEFAULT 1
+);
+GO
+
+-- 7. Tạo bảng WebcamRecord
+CREATE TABLE WebcamRecord (
+    webcam_record_id uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+    session_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Session(session_id),
+    student_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Account(account_id),
+    video_url NVARCHAR(500),
+    created_at DATETIME DEFAULT GETDATE(),
+    status INT DEFAULT 1
+);
+GO
+
+-- 8. Tạo bảng ClassStudent
+CREATE TABLE ClassStudent (
+    class_student_id INT PRIMARY KEY IDENTITY(1,1),
+    class_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Class(class_id),
+    student_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Account(account_id),
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME NULL,
+    status INT DEFAULT 1
+);
+GO
+
+-- 9. Tạo bảng Attendance
+CREATE TABLE Attendance (
+    attendance_id INT PRIMARY KEY IDENTITY(1,1),
+    session_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Session(session_id),
+    student_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Account(account_id),
+    status INT NULL DEFAULT 0,
+    img_url NVARCHAR(500),
+    attendance_time DATETIME DEFAULT GETDATE(),
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME NULL
+);
+GO
+
+-- 10. Tạo bảng Parent
+CREATE TABLE Parent (
+    parent_id uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+    student_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Account(account_id),
+    full_name NVARCHAR(100) NOT NULL,
+    phone_number NVARCHAR(20) NULL,
+    email NVARCHAR(100) NULL,
+    status INT DEFAULT 1
+);
+GO
+
+-- 11. Tạo bảng Notification
+CREATE TABLE Notification (
+    notification_id uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+    title NVARCHAR(255) NOT NULL,
+    content NVARCHAR(255) NOT NULL,
+    created_by uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Account(account_id),
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME NULL
+);
+GO
+
+--  11.1 Tạo bảng Permission
+CREATE TABLE Permission (
+    permission_id uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+    name NVARCHAR(100) NOT NULL UNIQUE,  -- Tên quyền (VD: 'manage_users', 'view_reports')
+    description NVARCHAR(255) NULL,      -- Mô tả quyền
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
+    status INT DEFAULT 1                 -- 1: Hoạt động, 0: Không hoạt động
+);
+GO
+
+-- 11.2 Tạo bảng User Permission
+CREATE TABLE UserPermission (
+    user_permission_id INT PRIMARY KEY IDENTITY(1,1),
+    account_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Account(account_id) ON DELETE CASCADE,
+    permission_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Permission(permission_id) ON DELETE CASCADE,
+    assigned_at DATETIME DEFAULT GETDATE(),
+    status INT DEFAULT 1                 -- 1: Đang có quyền, 0: Bị thu hồi
+);
+GO
+
+-- 12. Thêm dữ liệu mẫu cho Role
+INSERT INTO Role (name, description)
+VALUES 
+('Administrator', N'Quản lý hệ thống'),
+('Lecturer', N'Giảng dạy các lớp học'),
+('Student', N'Tham gia lớp học'),
+('Officer', N'Quản lý lớp học và lịch học');
+GO
+
+-- 12.1. Tạo bảng RefreshToken
+CREATE TABLE RefreshToken (
+    token_id uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+    account_id uniqueidentifier NOT NULL FOREIGN KEY REFERENCES Account(account_id) ON DELETE CASCADE,
+    token NVARCHAR(500) NOT NULL UNIQUE,
+    expires_at DATETIME NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    revoked_at DATETIME NULL,  -- Thời điểm token bị thu hồi (nếu có)
+    status INT DEFAULT 1        -- 1: Hoạt động, 0: Bị thu hồi
+);
+GO
+
+-- 13. Thêm dữ liệu mẫu cho Account
+INSERT INTO Account ( email, password, full_name, role_id, status, created_at)
+VALUES 
+( 'admin@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Nguyễn Văn Quân', (select role_id from Role where name = 'Administrator'), 1,  GETDATE()), --password: matkhau123
+( 'officer1@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Trần Thị Mai', (select role_id from Role where name = 'Officer'), 1,  GETDATE()), --password: matkhau123
+( 'officer2@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Phạm Hoàng Nam', (select role_id from Role where name = 'Officer'), 1,  GETDATE()), --password: matkhau123
+( 'lecturer1@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Lê Thanh Hải', (select role_id from Role where name = 'Lecturer'), 1,  GETDATE()), --password: matkhau123
+( 'lecturer2@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Nguyễn Thị Lan', (select role_id from Role where name = 'Lecturer'), 1,  GETDATE()), --password: matkhau123
+( 'lecturer3@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Doãn Minh Tài', (select role_id from Role where name = 'Lecturer'), 1,  GETDATE()), --password: matkhau123
+( 'student1@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Đỗ Quốc Đạt', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student2@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Bùi Văn Hiện', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student3@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Nguyễn Hoàng Linh', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student4@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Lương Thị Dân', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student5@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Phan Thanh Khánh', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student6@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Cao Văn Linh', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student7@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Tôn Nữ Minh', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student8@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Tô Minh NHật', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student9@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Huỳnh Văn Trọng', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student10@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Đặng Thị Phan', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student11@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Hoàng Minh Quân', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student12@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Ngô Văn Danh', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student13@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Vũ Thị Sinh', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student14@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Mai Thanh Tí', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student15@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Lý Minh Ưu', (select role_id from Role where name = 'Student'), 1,  GETDATE()), --password: matkhau123
+( 'student16@gmail.com', 'fc8d5c17ee6bd893ac3d47583df509da68ada40070b9c9e1890cae52bc62de28', N'Châu Văn Việt', (select role_id from Role where name = 'Student'), 1,  GETDATE()); --password: matkhau123
+
+-- 14. Thêm dữ liệu mẫu cho Course
+INSERT INTO Course (course_name, description, created_by, status, created_at)
+VALUES 
+(N'IELTS', N'Lớp học IELTS Tháng 2/25', (select account_id from account where email = 'admin@gmail.com'), 1, GETDATE()),
+(N'SAT', N'Lớp học SAT Tháng 2/25', (select account_id from account where email = 'admin@gmail.com'), 1, GETDATE());
+GO
+
+-- 15. Thêm lớp IELTS02-25 và SAT02-25
+INSERT INTO Class (class_code, class_name, course_id, total_session, start_date, end_date, status, created_at)
+VALUES 
+(N'IELTS02-25', N'Lớp IELTS Khai Giảng 02-25', 1, 15, GETDATE(), DATEADD(DAY, 60, GETDATE()), 0, GETDATE()),
+(N'SAT02-25', N'Lớp SAT Khai Giảng 02-25', 2, 15, GETDATE(), DATEADD(DAY, 60, GETDATE()), 0, GETDATE());
+GO
+
+-- 15. Xếp học viên vào lớp IELTS01-25
+INSERT INTO ClassStudent (class_id, student_id, status, created_at)
+SELECT 
+    (SELECT class_id FROM Class WHERE class_code = N'IELTS02-25'), account_id, 1, GETDATE()
+FROM (
+    SELECT account_id, ROW_NUMBER() OVER (ORDER BY created_at) AS row_num
+    FROM Account 
+    WHERE role_id = (select role_id from role where name = 'Student')
+) AS subquery
+WHERE row_num BETWEEN 1 AND 8;
+GO
+
+-- 16. Xếp học viên vào lớp SAT01-25
+INSERT INTO ClassStudent (class_id, student_id, status, created_at)
+SELECT 
+    (SELECT class_id FROM Class WHERE class_code = N'SAT02-25'), account_id, 1, GETDATE()
+FROM (
+    SELECT account_id, ROW_NUMBER() OVER (ORDER BY created_at) AS row_num
+    FROM Account 
+    WHERE role_id = (select role_id from role where name = 'Student')
+) AS subquery
+WHERE row_num BETWEEN 9 AND 16;
+GO
+
+-- 17. Thêm dữ liệu mẫu cho permission
+INSERT INTO Permission (name, description)
+VALUES 
+('officer:users', N'Quản lý người dùng'),
+('officer:classes', N'Quản lý lớp học'),
+('officer:courses', N'Quản lý khóa học'),
+('officer:sessions', N'Quản lý buổi học'),
+('officer:students', N'Quản lý học viên'),
+('officer:attendance', N'Quản lý học viên'),
+('view:reports', N'Xem báo cáo');
+GO
+
+-- 18. Thêm dữ liệu mẫu cho UserPermission
+INSERT INTO UserPermission(account_id, permission_id)
+VALUES
+((select account_id from account where email = 'officer1@gmail.com'), (SELECT permission_id FROM Permission WHERE name = 'officer:classes')),
+((select account_id from account where email = 'officer1@gmail.com'), (SELECT permission_id FROM Permission WHERE name = 'officer:sessions')),
+((select account_id from account where email = 'officer1@gmail.com'), (SELECT permission_id FROM Permission WHERE name = 'officer:students')),
+((select account_id from account where email = 'officer2@gmail.com'), (SELECT permission_id FROM Permission WHERE name = 'officer:courses')),
+((select account_id from account where email = 'officer2@gmail.com'), (SELECT permission_id FROM Permission WHERE name = 'officer:attendance')),
+((select account_id from account where email = 'officer2@gmail.com'), (SELECT permission_id FROM Permission WHERE name = 'officer:users'));
+GO
