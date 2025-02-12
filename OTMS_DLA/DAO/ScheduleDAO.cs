752cdb2cdde897d89f9087e69e4906dc2f09e3e3
quana <quanhdpt@gmail.com>
2025-02-12 10:43:25 +0700

    add scheduleDAO

---
+++ OTMS_DLA/DAO/ScheduleDAO.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTMS_DLA.DAO
+{
+    public class ScheduleDAO : GenericDAO<Account>
+    {
+        public ScheduleDAO(OtmsContext context) : base(context) { }
+    }
+}
