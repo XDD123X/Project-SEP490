3f6b369d4b59e1e1805fc85b72ebc8fb75b865f9
BlackZ <duchinh0306@gmail.com>
2025-03-19 18:41:23 +0700

    commit

--- OTMS.API/Controllers/Officer-Endpoint/ClassController.cs
+++ OTMS.API/Controllers/Officer-Endpoint/ClassController.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OTMS.DAL.Interface;
 
 namespace OTMS.API.Controllers.Officer_Endpoint
 {
-    [Route("api/[controller]")]
+    [Route("api/officer/[controller]")]
     [ApiController]
     public class ClassController : ControllerBase
     {
+        private readonly IClassRepository _classRepository;
+        private readonly IClassStudentRepository _classStudentRepository;
+        private readonly IMapper _mapper;
+        private readonly IAccountRepository _accountRepository;
+
+        public ClassController(IClassRepository classRepository, IClassStudentRepository classStudentRepository, IAccountRepository accountRepository, IMapper mapper)
+        {
+            _accountRepository = accountRepository;
+            _mapper = mapper;
+            _classRepository = classRepository;
+            _classStudentRepository = classStudentRepository;
+        }
+
     }
 }
